da6d778a600763764bc49393ef73e27a0e013781
lakshitanegi-30 <lakshitanegi3012@gmail.com>
2025-09-15 20:17:22 +0530

    Add files via upload

---
+++ Day_13/d13q2.c
@@ -0,0 +1,18 @@
+//Write a program to print numbers from 1 to n.
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    // Input a number
+    printf("Enter a number (n): ");
+    scanf("%d", &n);
+
+    // Loop from 1 to n
+    for (i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
