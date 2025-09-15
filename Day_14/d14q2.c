e5489de06ebff4499e4c56cb5ef385c42a4de6c2
lakshitanegi-30 <lakshitanegi3012@gmail.com>
2025-09-15 20:19:49 +0530

    Add files via upload

---
+++ Day_14/d14q2.c
@@ -0,0 +1,30 @@
+//Write a program to print the product of even numbers from 1 to n.
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int product = 1;
+
+    // Input a number
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    // Calculate product
+    for (i = 2; i <= n; i += 2) {
+        product *= i;
+    }
+
+    printf("%d (", product);
+
+    for (i = 2; i <= n; i += 2) {
+        printf("%d", i);
+        if (i + 2 <= n) {
+            printf(" * ");
+        }
+    }
+
+    printf(")\n");
+
+    return 0;
+}
