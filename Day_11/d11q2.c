e79cc0930f0e5b0ba9f685c45bfa96cff90f66b8
lakshitanegi-30 <lakshitanegi3012@gmail.com>
2025-09-15 20:10:19 +0530

    Add files via upload

---
+++ Day_11/d11q2.c
@@ -0,0 +1,26 @@
+//Write a program to find profit or loss percentage given cost price and selling price.
+#include <stdio.h>
+
+int main() {
+    float cp, sp, percent;
+
+    // Input cost price and selling price
+    printf("Enter Cost Price: ");
+    scanf("%f", &cp);
+
+    printf("Enter Selling Price: ");
+    scanf("%f", &sp);
+
+    // Check for profit or loss
+    if (sp > cp) {
+        percent = ((sp - cp) / cp) * 100;
+        printf("Profit Percentage = %.2f%%\n", percent);
+    } else if (cp > sp) {
+        percent = ((cp - sp) / cp) * 100;
+        printf("Loss Percentage = %.2f%%\n", percent);
+    } else {
+        printf("No Profit, No Loss.\n");
+    }
+
+    return 0;
+}
