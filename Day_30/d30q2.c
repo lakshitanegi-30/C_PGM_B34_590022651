e3ea49c57d94c9c1c84548660db6fc31d16a05f9
lakshitanegi-30 <lakshitanegi3012@gmail.com>
2025-09-27 09:32:54 +0530

    Add files via upload

---
+++ Day_30/d30q2.c
@@ -0,0 +1,27 @@
+//Count positive, negative, and zero elements in an array.
+
+#include <stdio.h>
+int main() {
+
+    int num,p = 0,n = 0,z = 0;
+    printf("Enter the number of elements: ");
+    scanf("%d",&num);
+    int arr[num];
+
+    for(int i = 0;i < num;i++){
+    scanf("%d", &arr[i]);
+    }
+   for (int i = 0; i < num; i++) {
+        if (arr[i] > 0 )
+            p++;
+        else if (arr[i] < 0 )
+            n++;
+        else
+            z++;
+    }
+     printf("Positive = %d\n", p);
+     printf("Negative = %d\n", n);
+     printf("Zero = %d\n", z);
+
+   return 0;
+}
